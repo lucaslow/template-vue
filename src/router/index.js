1814db524f207adda1eb6eb3c4da6e876cc0ed5c
lucaslow <974860016@qq.com>
2020-12-19 18:01:33 +0800

    refactor(router/index.js): 懒加载Home组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from 'views/Home.vue'
+// import Home from 'views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -13,8 +13,8 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    // component: () => import(/* webpackChunkName: "Home" */'views/Home.vue')
-    component: Home
+    component: () => import(/* webpackChunkName: "Home" */ 'views/Home.vue')
+    // component: Home
   },
   {
     path: '/test',
